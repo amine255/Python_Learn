60bbb6305c1ca5d158c222ff6af2f9166863e9af
Amine Bennani <amine255@users.noreply.github.com>
2018-03-07 11:03:20 +0100

    Update CodingBatBest.py

--- CodingBatBest.py
+++ CodingBatBest.py
@@ -1,4 +1,4 @@
-# 1
+# 1.
 # Warmup-1 > sleep_in
 # The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 
